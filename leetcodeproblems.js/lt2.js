cf7d21dd9ffec768316183362a7b2a50c5db247f
Tech. Billionaire <fadhlillah949699@gmail.com>
2021-01-05 23:52:23 +0700

    Create lt2.js

---
+++ leetcodeproblems.js/lt2.js
@@ -0,0 +1,9 @@
+//!Array Partition I
+var arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let result = 0;
+  for (let i = 0; i < nums.length; i = i + 2) {
+    result += nums[i];
+  }
+  return result;
+};
