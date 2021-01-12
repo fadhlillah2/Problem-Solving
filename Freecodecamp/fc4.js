7cc6969d5e1f59233b413aea17e6a17e689e5a08
Tech. Billionaire <fadhlillah949699@gmail.com>
2021-01-12 17:24:36 +0700

    Update fc4.js

--- Freecodecamp/fc4.js
+++ Freecodecamp/fc4.js
@@ -1,6 +1,13 @@
-function bouncer(arr) {
-  let newArr = arr.filter(Boolean);
-  return newArr;
+function getIndexToIns(arr, num) {
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+
+  for (var a = 0; a < arr.length; a++) {
+    if (arr[a] >= num) return a;
+  }
+
+  return arr.length;
 }
 
-console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35));
