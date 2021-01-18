003f42c26ccaf0a37718c0e05f2315288da6f349
Tech. Billionaire <fadhlillah949699@gmail.com>
2021-01-18 14:58:12 +0700

    Delete fc4.js

--- Freecodecamp/fc4.js
+++
@@ -1,8 +0,0 @@
-// !Falsy Bouncer
-
-function bouncer(arr) {
-  return arr.filter(Boolean);
-}
-
-console.log(bouncer([7, "ate", "", false, 9]));
-console.log(bouncer([false, null, 0, NaN, undefined, ""]));
