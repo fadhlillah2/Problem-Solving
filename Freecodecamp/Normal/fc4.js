db08f8ba42720a5fce803c1498146a1a6f1483e3
Tech. Billionaire <fadhlillah949699@gmail.com>
2021-01-18 14:58:22 +0700

    Create fc4.js

---
+++ Freecodecamp/Normal/fc4.js
@@ -0,0 +1,8 @@
+// !Falsy Bouncer
+
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
+
+console.log(bouncer([7, "ate", "", false, 9]));
+console.log(bouncer([false, null, 0, NaN, undefined, ""]));
