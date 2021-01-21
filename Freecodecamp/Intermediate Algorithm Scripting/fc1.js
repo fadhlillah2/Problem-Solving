363f826b1abc16ada74737b99dd7ec062e367245
Tech. Billionaire <fadhlillah949699@gmail.com>
2021-01-22 00:49:32 +0700

    Update fc1.js

---
+++ Freecodecamp/Intermediate Algorithm Scripting/fc1.js
@@ -0,0 +1,20 @@
+//!Smallest Common Multiple
+const smallestCommons = (arr) => {
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  // Initially the solution is assigned to the highest value of the array
+  let sol = max;
+
+  for (let i = max - 1; i >= min; i--) {
+    // Each time the solution checks (i.e. sol%i===0) it won't be necessary
+    // to increment 'max' to our solution and restart the loop
+    if (sol % i) {
+      sol += max; //???
+      i = max;
+    }
+  }
+  return sol;
+};
+
+// test here
+console.log(smallestCommons([6, 7]));
