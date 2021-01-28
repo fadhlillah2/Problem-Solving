3effe4b8389722e13a2811cf79a4d679bb55b634
Tech. Billionaire <fadhlillah949699@gmail.com>
2021-01-29 00:53:30 +0700

    Update note2.js

--- Exercise/note2.js
+++ Exercise/note2.js
@@ -2,21 +2,23 @@
 // // Output: [0,1]
 // // Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 // var twoSum = function (nums, target) {
-//     var map = {};
-//     for (var i = 0; i < nums.length; i++) {
-//       var n = nums[i];
-//       if (map[target - n] !== undefined) {
-//         //jika map[target - n] terdefinisi atau ditemukan
-//         return [map[target - n], i];
-//         //maka tampilkan sebuah arr berisi [map[target - n], i]
-//       } else {
-//         map[n] = i;
-//         console.log(map[n]);
-//         //jika tidak maka d belakang layar tetap berjalan sebuah assignment
-//         //terhadap tiap2 key dari map(map={})
-//       }
+//   var map = {};
+//   for (var i = 0; i < nums.length; i++) {
+//     var n = nums[i];
+//     if (map[target - n] !== undefined) {
+//       //jika map[target - n] terdefinisi atau ditemukan. Dari sini kita tahu bahwa utk mentakan sebuah
+//       //pernyataan  yg terdefinisi komputer hanya mengerti menggunakan syntax if(expression !== undefined)
+//       //bukan defined.
+//       return [map[target - n], i];
+//       //maka tampilkan sebuah arr berisi [map[target - n], i]
+//     } else {
+//       map[n] = i;
+//       console.log(map[n]);
+//       //jika tidak maka d belakang layar tetap berjalan sebuah assignment
+//       //terhadap tiap2 key dari map(map={})
 //     }
-//     console.error("No two sum solution");
-//   };
+//   }
+//   console.error("No two sum solution");
+// };
 
-//   console.log(twoSum([2, 11, 7, 15], 9));
+// console.log(twoSum([2, 11, 7, 15], 9));
