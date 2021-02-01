53fa6f80e80d81e9336ac616965b209debafcef6
Tech. Billionaire <fadhlillah949699@gmail.com>
2021-02-02 01:21:34 +0700

    Update note2.js

--- Exercise/note2.js
+++ Exercise/note2.js
@@ -23,7 +23,7 @@
 
 // console.log(twoSum([2, 11, 7, 15], 9));
 
-//!=======================================================================================================
+//!=======================================================================================================1
 
 // function fizzBuzz2(n) {
 
@@ -40,7 +40,7 @@
 
 //   console.log(fizzBuzz2(15));
 
-//!=====================================================================================================
+//!=====================================================================================================2
 
 // var inverseString = function (param) {
 //   var l = param.length - 1,
@@ -57,7 +57,7 @@
 // };
 // console.log(inverseString("abcd")); //??????????? masih perlu revisi
 
-//! ========================================================================================================
+//! ========================================================================================================3
 
 // function confirmEnding(str, target) {
 //   for (
@@ -73,7 +73,7 @@
 // }
 // console.log(confirmEnding("cobakan", "akan"));
 
-//!===========================================================================================================
+//!===========================================================================================================4
 
 // Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
 // Output: [0,1,2,3,4]
@@ -104,3 +104,25 @@
 //   console.log(b);
 
 //   console.log(!undefined === ![][0]);
+
+//!==================================================================================================================5
+
+// Input: s = "RLLLLRRRLR"
+// Output: 3
+// Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
+// var balancedStringSplit = function (s) {
+//     const charge = { L: -1, R: 1 };
+//     let sum = 0,
+//       c = 0;
+//     for (let i = 0; i < s.length; i++) {
+//       sum += charge[s[i]];
+//       //untuk menentukan apakah elemen s bernilai 1 atau -1
+//       if (sum == 0) c++;
+//     }
+//     return c;
+//   };
+//intinya disini huruf L bernilai -1 dan huruf R brnnilai +1 sehingga apabila menghalikan nilai 0 instruksikan komputer
+//untuk mengeluarkan output increment (penjumlahan).
+//contohnya sebagai berikut "RL"= +1 -1 = 0 maka terhitung 1 dan begitu seterusnya
+//   console.log(balancedStringSplit("RLRRLLRLRL"));
