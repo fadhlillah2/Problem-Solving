f8788e690d4af8aeb7266a5dc631242bc6b476e9
Tech. Billionaire <fadhlillah949699@gmail.com>
2021-02-13 02:25:38 +0700

    Update note4.js

--- Exercise/note4.js
+++ Exercise/note4.js
@@ -72,5 +72,29 @@
 //   console.log(0 ^ 2 ^ 4 ^ 6 ^ 8);
 
 //! #3 =======================================================================================================
+
+//! Permutator
+// const permutator = (inputArr) => {
+//     let result = [];
+
+//     const permute = (arr, m = []) => {
+//       if (arr.length === 0) {
+//         result.push(m);
+//       } else {
+//         for (let i = 0; i < arr.length; i++) {
+//           let curr = arr.slice();
+//           let next = curr.splice(i, 1);
+//           permute(curr.slice(), m.concat(next));
+//         }
+//       }
+//     };
+
+//     permute(inputArr);
+
+//     return result;
+//   };
+
+//   console.log(permutator([]));
+
 //! #4 =======================================================================================================
 //! #5 =======================================================================================================
