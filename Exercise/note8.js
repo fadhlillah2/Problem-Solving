a8f89df45352cb7c8337c4784da272ff130a50a3
Tech. Billionaire <fadhlillah949699@gmail.com>
2021-03-01 09:10:32 +0700

    Update note8.js

--- Exercise/note8.js
+++ Exercise/note8.js
@@ -82,4 +82,34 @@
 //   console.log(findXor([0, 1, 2, 3, 4, 6]));
 
 //! #4 ==========================================================================================
+
+//! Height Checker
+// Example 1:
+// Input: heights = [1,1,4,2,1,3]
+// Output: 3
+// Explanation:
+// Current array : [1,1,4,2,1,3]
+// Target array  : [1,1,1,2,3,4]
+// On index 2 (0-based) we have 4 vs 1 so we have to move this student.
+// On index 4 (0-based) we have 1 vs 3 so we have to move this student.
+// On index 5 (0-based) we have 3 vs 4 so we have to move this student.
+
+// Example 2:
+// Input: heights = [5,1,2,3,4]
+// Output: 5
+
+// Example 3:
+// Input: heights = [1,2,3,4,5]
+// Output: 0
+
+// var heightChecker = function (heights) {
+//   console.log(heights);
+//   let etalon = [...heights].sort((a, b) => a - b);
+//   console.log(etalon);
+//   let count = 0;
+//   etalon.forEach((a, b) => (heights[b] !== etalon[b] ? count++ : 0));
+//   return count;
+// };
+// console.log(heightChecker([1, 1, 4, 2, 1, 3]));
+
 //! #5 ==========================================================================================
