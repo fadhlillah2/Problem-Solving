dbb9387f1f0cef9371c71aa34b139b86ea7931c2
fadhlillah2 <fadhlillah949699@gmail.com>
2021-03-15 14:30:11 +0700

    Update note12.js

--- Exercise/note12.js
+++ Exercise/note12.js
@@ -13,7 +13,7 @@ var countConsistentStrings = function (allowed, words) {
 };
 console.log(countConsistentStrings("ab", ["ad", "bd", "aaab", "baa", "badab"]));
 
-//! ==================================================================================
+//! 1==================================================================================
 
 var subtractProductAndSum = function (n) {
   let sum = 0,
@@ -27,3 +27,32 @@ var subtractProductAndSum = function (n) {
   return product - sum;
 };
 console.log(subtractProductAndSum(234));
+
+//! 2 ==================================================================================
+
+// var intersect = function (num1, num2) {
+//   const map = new Map();
+//   console.log(map);
+//   for (const n of num1) {
+//     if (map.has(n)) {
+//       map.set(n, map.get(n) + 1);
+//     } else {
+//       map.set(n, 1);
+//     }
+//   }
+//   console.log(map);
+//   const result = [];
+//   for (const n of num2) {
+//     if (map.has(n) && map.get(n) > 0) {
+//       result.push(n);
+//       map.set(n, map.get(n) - 1);
+//     }
+//   }
+//   console.log(map);
+//   return result;
+// };
+// console.log(intersect([1, 2, 1, 2, 3, 5, 2, 5, 1], [1, 2, 1]));
+
+//! 2 ==================================================================================
+//! 2 ==================================================================================
+//! 2 ==================================================================================
