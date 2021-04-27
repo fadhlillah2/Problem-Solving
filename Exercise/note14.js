5798bc8ca4e7198fb2b82b76e5a7c2377e4091de
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-27 14:51:36 +0700

    Update note14.js

--- Exercise/note14.js
+++ Exercise/note14.js
@@ -139,3 +139,33 @@ console.log(
 //   return a;
 // };
 // console.log(removeOuterParentheses("()()"));
+
+// ! 4 ====================================================================================================
+
+//* Maximum 69 Number
+
+// Example 1:
+// Input: num = 9669
+// Output: 9969
+// Explanation:
+// Changing the first digit results in 6669.
+// Changing the second digit results in 9969.
+// Changing the third digit results in 9699.
+// Changing the fourth digit results in 9666.
+// The maximum number is 9969.
+
+// Example 2:
+// Input: num = 9996
+// Output: 9999
+// Explanation: Changing the last digit 6 to 9 results in the maximum number.
+
+// Example 3:
+// Input: num = 9999
+// Output: 9999
+// Explanation: It is better not to apply any change.
+
+// const maximum69Number = (num) => Number(num.toString().replace("6", "9"));
+// console.log(maximum69Number(9669));
+
+// let a = 123123123123;
+// console.log(Number(a.toString().replace("1", "5")));
