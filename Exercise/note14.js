264c4a74debd02dbbe796b3900c9a435828952af
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-29 18:07:24 +0700

    Update note14.js

--- Exercise/note14.js
+++ Exercise/note14.js
@@ -169,3 +169,27 @@ console.log(
 
 // let a = 123123123123;
 // console.log(Number(a.toString().replace("1", "5")));
+
+
+// ! 5 ====================================================================================================
+
+// Input: n = 234
+// Output: 15
+// Explanation:
+// Product of digits = 2 * 3 * 4 = 24
+// Sum of digits = 2 + 3 + 4 = 9
+// Result = 24 - 9 = 15
+
+// var subtractProductAndSum = function (n) {
+//   let sum = 0,
+//     product = 1;
+//   while (n) {
+//     num = n % 10;
+//     sum += num;
+//     product *= num;
+//     n = Math.floor(n / 10);
+//   }
+//   return product - sum;
+// };
+// console.log(subtractProductAndSum(234));
+
