76a3e1198817c19ba5c885eb59a55d764824bf20
fadhlillah2 <fadhlillah949699@gmail.com>
2021-05-01 07:44:43 +0700

    Update note14.js

--- Exercise/note14.js
+++ Exercise/note14.js
@@ -193,3 +193,30 @@ console.log(
 // };
 // console.log(subtractProductAndSum(234));
 
+
+
+
+
+
+// Input: n = 7
+// Output: 6
+// Explanation: Details of the tournament:
+// - 1st Round: Teams = 7, Matches = 3, and 4 teams advance.
+// - 2nd Round: Teams = 4, Matches = 2, and 2 teams advance.
+// - 3rd Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
+// Total number of matches = 3 + 2 + 1 = 6.
+
+// var numberOfMatches = function (n) {
+//   let sum = 0;
+//   while (n > 1) {
+//     if (n % 2 == 0) {
+//       sum += n / 2;
+//       n = n / 2;
+//     } else {
+//       sum += (n - 1) / 2;
+//       n = (n - 1) / 2 + 1;
+//     }
+//   }
+//   return sum;
+// };
+// console.log(numberOfMatches(7));
