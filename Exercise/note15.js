1dcf2f31a9a8bd3328183e1a25071d4c9fb59053
FADHLILLAH <73236007+fadhlillah2@users.noreply.github.com>
2021-06-29 23:40:01 +0700

    Update note15.js

--- Exercise/note15.js
+++ Exercise/note15.js
@@ -114,3 +114,14 @@
 var checkIfPangram = function(sentence) {
     return new Set(sentence.split("")).size == 26;
 };
+
+var longestCommonPrefix = function(strs) {
+    'use strict';
+    if (strs === undefined || strs.length === 0) { return ''; }
+    
+    return strs.reduce((prev, next) => {
+        let i = 0;
+        while (prev[i] && next[i] && prev[i] === next[i]) i++;
+        return prev.slice(0, i);
+    });
+};
