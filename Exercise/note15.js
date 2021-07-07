0cb5c348095d31315b0bdc651ee51bda9876be76
FADHLILLAH <73236007+fadhlillah2@users.noreply.github.com>
2021-07-08 02:31:00 +0700

    Update note15.js

--- Exercise/note15.js
+++ Exercise/note15.js
@@ -139,3 +139,15 @@ function pigLatin(str){
    
 }
 console.log(pigLatin('dante'));
+
+
+
+
+
+function twoSum (arr, target) {
+    let idx = [];
+    for(let i = arr.length-1; i >= 0; i++){
+        if(target > arr[i]) console.log(indexOf(target - arr[i]))
+    }
+}
+console.log(twoSum([2,7,11,15],9))
