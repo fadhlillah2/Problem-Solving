7fed17cfc94018b931bbd49d1512c61d910437ef
FADHLILLAH <73236007+fadhlillah2@users.noreply.github.com>
2021-07-09 09:05:27 +0700

    Update note16.js

--- Exercise/note16.js
+++ Exercise/note16.js
@@ -11,3 +11,20 @@ function twoSum (arr, target) {
     
 }
 console.log(twoSum([2,7,11,15],18))
+
+
+
+var findTheDifference = function(s, t) {
+    let sum1 = 0;
+    for (let i = 0; i < s.length; i++) {
+      sum1 += s[i].charCodeAt();
+    }
+    
+    let sum2 = 0;
+    for (let i = 0; i < t.length; i++) {
+      sum2 += t[i].charCodeAt();
+    }
+    
+    return String.fromCharCode(sum2-sum1);
+  };
+  console.log(findTheDifference('abcd','abcde'))
